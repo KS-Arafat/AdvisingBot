cd713895e60203a933b1089f8e0be7007fc829e0
KS-Arafat <sfnarafat@gmail.com>
2023-07-21 09:23:39 +0600

    Scrapper for getting saved courselist, readvising count and necessary headers for saveadvising http request

---
+++ Scraping/scraper.js
@@ -0,0 +1,79 @@
+import { advisingURL } from "../Directories-URLS/URLS.js";
+
+import FormData from "form-data";
+import puppeteer from "puppeteer-core";
+
+import * as env from "dotenv";
+
+env.config();
+if (process.env.ID_hash == undefined) {
+	console.error(".env file needed ");
+	process.abort();
+}
+
+const LaunchScraper = async (cookie = "") => {
+	const form = new FormData();
+	const Browser = await puppeteer.launch({
+		headless: true,
+		timeout: 10000,
+		executablePath: process.env.browserPath,
+	});
+	const cookies = [
+		{
+			name: "PHPSESSID",
+			value: cookie.split("=")[1],
+			domain: "rds3.northsouth.edu",
+			path: "/",
+			expires: -1,
+			size: 35,
+			httpOnly: false,
+			secure: false,
+			session: true,
+			sameParty: false,
+			sourceScheme: "Secure",
+			sourcePort: 443,
+		},
+	];
+	let myCourese = "";
+	let readvNo = "";
+	try {
+		const page = await Browser.newPage();
+
+		await page.setCookie(...cookies);
+		await page.goto(advisingURL, { waitUntil: "networkidle0" });
+		readvNo = await page.$eval("#readvising", (el) => el.value); // got the readvising value
+
+		page.on("console", async (msg) => {
+			const msgArgs = msg.args();
+			myCourese = await msgArgs[0].jsonValue();
+			myCourese = myCourese.toString().replace("0,0,0,0;", "");
+		});
+		await page.evaluate(() => {
+			let mycrs = "";
+			advisedArray.forEach(
+				(e) =>
+					(mycrs +=
+						e["courseCode"] +
+						"," +
+						e["section"] +
+						"," +
+						e["courseCredit"] +
+						"," +
+						e["fees"] +
+						";")
+			);
+			console.log(mycrs);
+		});
+	} catch (error) {
+		console.error(error);
+	}
+	setTimeout(async () => {
+		await Browser.close();
+	}, 3000);
+	form.append("studentID", process.env.ID);
+	form.append("courses", myCourese);
+	form.append("readvising", readvNo);
+	return form;
+};
+
+export default LaunchScraper;
---
+++ httpReq/saveAdvising.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import FormData from "form-data";
+import { saveAdvisingURL } from "../Directories-URLS/URLS.js";
+
+const saveAdvsing = async (form = new FormData(), cookie) => {
+	const config = {
+		method: "post",
+		url: saveAdvisingURL,
+		headers: {
+			Cookie: cookie,
+			...form.getHeaders(),
+		},
+		data: form,
+	};
+
+	return axios(config).then((res) => {
+		if (res.status == 200) {
+			return {
+				msg: res.data,
+				course: form,
+				success: true,
+			};
+		} else return { msg: res.data, success: false };
+	});
+};
+
+export default saveAdvsing;

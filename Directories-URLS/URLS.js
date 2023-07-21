0284fa98c960613dd8a85b9f0e1eae92b527c8c6
KS-Arafat <sfnarafat@gmail.com>
2023-07-21 09:27:25 +0600

    One URL added(saveAdvisingURL)

--- Directories-URLS/URLS.js
+++ Directories-URLS/URLS.js
@@ -2,3 +2,5 @@ export const captchaURL = "https://rds3.northsouth.edu/index.php/captcha";
 export const authURL = "https://rds3.northsouth.edu/index.php/common/login";
 export const advisingURL =
 	"https://rds3.northsouth.edu/index.php/students/advising";
+export const saveAdvisingURL =
+	"https://rds3.northsouth.edu/index.php/students/advising/saveadvising/";
